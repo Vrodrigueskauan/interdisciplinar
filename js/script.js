6a9d1966f55d967e5d1c12d9af64f0b4a5ba042d
vrodrigueskauan <kauanvenanciobarata@gmail.com>
2024-10-10 10:01:12 -0300

    aaaa

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.querySelector('.fade-in');
+    title.style.animationDelay = '0.5s'; // Ajusta o tempo de delay se necessário
+});
